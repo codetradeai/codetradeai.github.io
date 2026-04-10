1097394c399211179bf6e3cae22838b90fd8d789
Joao Tomas <joaotomasvenancio@gmail.com>
2026-04-10 16:22:57 -0400

    refactor: clean up CSS, optimize theme toggling logic, update professional summary, and remove obsolete layout automation scripts

--- translate.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open("index.html", "r") as f:
-    content = f.read()
-
-# I will write a simple translation mapping for you, but it's massive.
-# Let's save a copy of index.html first.
-with open("index-en.html", "w") as f:
-    f.write(content.replace("lang=\"pt-BR\"", "lang=\"en-US\""))
